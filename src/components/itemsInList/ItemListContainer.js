13e638b35e8ed0e85f81195088b13d5414d256f0
Felipe Puiggari Medici <felipepuiggarimedici@gmail.com>
2022-07-02 19:09:10 -0300

    Added the change in categories

--- src/components/itemsInList/ItemListContainer.js
+++ src/components/itemsInList/ItemListContainer.js
@@ -1,22 +1,19 @@
 import React, { useEffect, useState }  from "react";
-import { useParams } from "react-router-dom";
 import getData from "../../helpers/getData";
 import ItemList from "./ItemList";
 import "./../../styles/itemCards/cardContainer.scss";
 import "./../../styles/itemCards/purchaseInterface.scss";
-import ItemCount from "./ItemCount";
 import LoadingScreen from "../LoadingScreen";
+import {useParams} from "react-router-dom";
 
 const ItemListContainer = () => {
     const [loading, setLoading]= useState(true);
     const [gameList, setGameList] = useState();
-
     const routingInfo = useParams();
-    console.log(routingInfo)
 
     const getGames = async() => {
         try {
-            const response = await getData;
+            const response = await getData(routingInfo);
             setGameList(response);
         }catch(error) {
             console.log("Data not found")
@@ -25,18 +22,14 @@ const ItemListContainer = () => {
         }
     }
     useEffect(() => {
+        setLoading(true);
         getGames();
-    }, []);
+        // eslint-disable-next-line
+    }, [routingInfo]);
 
-    const onAdd = () => {
-        console.log("Added to cart")
-    }
     return <>
         <div className="itemListContainer">
             {loading ? <LoadingScreen/> : <ItemList gameList = {gameList}/>}
-        </div>
-        <div>
-            <ItemCount initial={1} stock={10} onAdd={onAdd}/>
         </div>
     </>
 }
--- src/helpers/getData.js
+++ src/helpers/getData.js
@@ -1,12 +1,19 @@
-const getData = new Promise((resolve, reject) => {
-    let condition = true
-    setTimeout(()=>{
-      if(condition){
-        resolve(require("./../data/games.json"))
-      }else{
-        reject('Games not found')
-      }
-    }, 2000)
-});
+const getData = (currentCategory) => {
+    return new Promise((resolve, reject) => {
+      let condition = true;
 
+      setTimeout(()=>{
+        if(condition){
+          if (Object.entries(currentCategory).length === 0) {
+            resolve(require("./../data/games.json"))
+          }
+          else {
+            resolve(require("./../data/games.json").filter((game) => game.categories.includes(currentCategory.categoryName)));
+          }
+        }else{
+          reject('Games not found')
+        }
+      }, 2000)
+  });
+}
 export default getData;
